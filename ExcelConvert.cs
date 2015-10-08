5b8bab8150f5a4f713e7a3a5de20dd8d04a7df59
Gyuha Shin <nicegyuha@gmail.com>
2015-10-08 14:15:17 +0900

    xlsx to csv convert

---
+++ ExcelConvert.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel;
+
+namespace xlsxToCsv
+{
+    public class ExcelConvert
+    {
+        DataSet result = new DataSet();
+
+        public bool convert(string src, string tar)
+        {
+            getExcelData(src);
+            return converToCSV(tar);
+        }
+
+        private void getExcelData(string file)
+        {
+            if (file.EndsWith(".xlsx"))
+            {
+                // Reading from a binary Excel file (format; *.xlsx)
+                FileStream stream = File.Open(file, FileMode.Open, FileAccess.Read);
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                result = excelReader.AsDataSet();
+                excelReader.Close();
+            }
+
+            if (file.EndsWith(".xls"))
+            {
+                // Reading from a binary Excel file ('97-2003 format; *.xls)
+                FileStream stream = File.Open(file, FileMode.Open, FileAccess.Read);
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                result = excelReader.AsDataSet();
+                excelReader.Close();
+            }
+
+            List<string> items = new List<string>();
+            for (int i = 0; i < result.Tables.Count; i++)
+                items.Add(result.Tables[i].TableName.ToString());
+        }
+
+        private bool converToCSV(string toFilePath)
+        {
+            int index = 0;
+            // sheets in excel file becomes tables in dataset
+            // result.Tables[0].TableName.ToString(); // to get sheet name (table name)
+
+            string a = "";
+            int row_no = 0;
+            if(result.Tables.Count == 0)
+            {
+                return false;
+            }
+            while (row_no < result.Tables[index].Rows.Count)
+            {
+                for (int i = 0; i < result.Tables[index].Columns.Count; i++)
+                {
+                    a += result.Tables[index].Rows[row_no][i].ToString() + ",";
+                }
+                row_no++;
+                a += "\n";
+            }
+            string output = toFilePath;
+            StreamWriter csv = new StreamWriter(@output, false);
+            csv.Write(a);
+            csv.Close();
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xlsxToCsv
+{
+    class Program
+    {
+        static ExcelConvert excelConvert = new ExcelConvert();
+
+        static int fileList()
+        {
+            var allfiles = System.IO.Directory.GetFiles(".", "*.*", System.IO.SearchOption.AllDirectories).Where(s => s.EndsWith(".xls") || s.EndsWith(".xlsx"));
+            if ( allfiles.Count() == 0)
+            {
+                Console.WriteLine("File not found..");
+                return 1;
+            }
+            foreach (string src in allfiles)
+            {
+                var tar = Path.ChangeExtension(src, "csv");
+                Console.WriteLine(src + " => " + tar);
+                try {
+                    if(excelConvert.convert(src, tar) == false)
+                    {
+                        Console.WriteLine("ERROR : ["+src+"] can't read excel file.");
+                    }
+                } catch( InvalidCastException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            return 0;
+        }
+
+        static void printHelp()
+        {
+            string filename = Process.GetCurrentProcess().ProcessName;
+            Console.WriteLine("Excel to CSV(UTF-8).  \n\r");
+            Console.WriteLine("USING 1.");
+            Console.WriteLine("\t"+ filename);
+            Console.WriteLine("\n\rUSING 2.");
+            Console.WriteLine("\t"+ filename +" [Source FilePath] [Target FilePath] [-h]\n\r");
+            Console.WriteLine("\t  -h\t Display help");
+        }
+
+        static int Main(string[] args)
+        {
+            foreach(string a in args )
+            {
+                if( String.Compare(a, "-h", true) == 0)
+                {
+                    printHelp();
+                    return 0;
+                }
+            }
+
+            if (args.Length != 0 && args.Length != 2)
+            {
+                printHelp();
+                return 1;
+            }
+
+            // 폴더를 검색해서 변경
+            if (args.Length == 0)
+            {
+                return fileList();
+            }
+            
+            // cli file change
+            string srcFile = args[0];
+            string tarFile = args[1];
+
+            if(!File.Exists(srcFile))
+            {
+                Console.WriteLine("File not found. [" + srcFile + "]");
+                return 1;
+            }
+
+            string tarPath = Path.GetDirectoryName(tarFile);
+            if (!Directory.Exists(tarPath) && tarPath.Length > 0)
+            {
+                Console.WriteLine("Target Folder not exist.");
+                return 1;
+            }
+
+            Console.WriteLine(srcFile + " => " + tarFile);
+            if (excelConvert.convert(srcFile, tarFile) == false)
+            {
+                Console.WriteLine("ERROR : [" + srcFile + "] can't read excel file.");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
